81c3d412e3037948079cf0eea968f9c4e0153a09
AunCly <aurelien.clugery@gmail.com>
2017-04-21 19:57:57 +0200

    divers

--- sleep_led.py
+++ sleep_led.py
@@ -6,24 +6,47 @@ import time
 GPIO.setmode(GPIO.BCM) 
 GPIO.setup(4, GPIO.OUT)
  
-# creation d'un objet PWM. canal=4 frequence=50Hz
+# create PWM object. gpio=4 freq=50Hz
 LED = GPIO.PWM(4, 50)
  
-# demarrage du PWM avec un cycle a 0 (LED off)
+# start PWM cycle with 0 (LED off)
 LED.start(0)
  
-# On fait varier le rapport cyclique de 0 a 100 puis de 100 a 0
+global_duration_m = 20
+global_duration_s = 20*60
+nb_repetition_start = 11
+nb_repetition_end = 6
+nb_repetition_current = 11
+time_per_repetion_cycle = global_duration_s/6
+time_start = time.time()
+
+# Cyclic ratio is varied 0 to 100 and then 100 to 0
+print('------- Debut du test ---------')
+rep = 0
+nb_cycle=1
 try:
-    while 1:
-        for dc in range(0, 101, 1):
-            LED.ChangeDutyCycle(dc)
-            time.sleep(0.0272)
-        for dc in range(100, -1, -1):
-            LED.ChangeDutyCycle(dc)
-            time.sleep(0.0272)
+	while nb_repetition_current > 5:
+	    sleep_time_dc_cycle = ((((60/nb_repetition_current)/2))/100)
+	    sleep_time_dc_cycle = sleep_time_dc_cycle-(sleep_time_dc_cycle/200)
+
+	    for dc in range(1, 101, 1):
+	        LED.ChangeDutyCycle(dc)
+	        time.sleep(sleep_time_dc_cycle)
+
+	    for dc in range(99, -1, -1):
+	        LED.ChangeDutyCycle(dc)
+	        time.sleep(sleep_time_dc_cycle)
+	    rep = rep+1
+	    if time.time() > time_start+(nb_cycle*time_per_repetion_cycle) :
+	        print("-- Fin du cycle de "+str(nb_repetition_current)+" Rep / minutes --")
+	        print("-- Nb de rep pour le cycle "+str(rep)+"  --")
+	        nb_repetition_current = nb_repetition_current-1
+	        rep = 0
+	        nb_cycle = nb_cycle+1
+            
 except KeyboardInterrupt:
     pass
-
+    
 LED.stop()
 
 GPIO.cleanup()
--- test_sleep_led.py
+++ test_sleep_led.py
@@ -16,32 +16,35 @@ global_duration_m = 20
 global_duration_s = 20*60
 nb_repetition_start = 11
 nb_repetition_end = 6
-nb_repetition_current = 6
-time_per_repetion_cycle = global_duration_s/(nb_repetition_start-nb_repetition_end)
-
-#time_start = time.time()
+nb_repetition_current = 11
+time_per_repetion_cycle = global_duration_s/6
+time_start = time.time()
 
 # Cyclic ratio is varied 0 to 100 and then 100 to 0
-
+print('------- Debut du test ---------')
+rep = 0
+nb_cycle=1
 #try:
 while nb_repetition_current > 5:
     sleep_time_dc_cycle = ((((60/nb_repetition_current)/2))/100)
-    #sleep_time_dc_cycle = sleep_time_dc_cycle-(sleep_time_dc_cycle/200)
+    sleep_time_dc_cycle = sleep_time_dc_cycle-(sleep_time_dc_cycle/200)
     #sleep_time_dc_cycle = 0.0495
-    #print("sleep_time = "+str(sleep_time_dc_cycle))
-    time_start = time.time()
     for dc in range(1, 101, 1):
         #LED.ChangeDutyCycle(dc)
         #print(dc);
         time.sleep(sleep_time_dc_cycle)
+
     for dc in range(99, -1, -1):
         #LED.ChangeDutyCycle(dc)
         #print(dc);
         time.sleep(sleep_time_dc_cycle)
-    print(str(time.time()-time_start))
-    break;
-        #if time.time() > time_start+time_per_repetion_cycle :
-            #nb_repetition_current = nb_repetition_current-1;
+    rep = rep+1
+    if time.time() > time_start+(nb_cycle*time_per_repetion_cycle) :
+        print("-- Fin du cycle de "+str(nb_repetition_current)+" Rep / minutes --")
+        print("-- Nb de rep pour le cycle "+str(rep)+"  --")
+        nb_repetition_current = nb_repetition_current-1
+        rep = 0
+        nb_cycle = nb_cycle+1
             
 #except KeyboardInterrupt:
     #pass
